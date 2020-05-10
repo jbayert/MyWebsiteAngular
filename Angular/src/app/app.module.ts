727b159e115a6440c358b6c5077696b9fc2253cb
jbayert <35811092+jbayert@users.noreply.github.com>
2020-05-10 16:47:38 -0500

    PaperKitTheme

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,31 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app.routing';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
-import { FooterComponent } from './shared/footer/footer.component';
-
-import { ComponentsModule } from './components/components.module';
-import { ExamplesModule } from './examples/examples.module';
-
+import { NavbarComponent } from './paperkit/shared/navbar/navbar.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    FooterComponent
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule,
-    FormsModule,
     RouterModule,
-    ComponentsModule,
-    ExamplesModule,
     AppRoutingModule,
   ],
   providers: [],
