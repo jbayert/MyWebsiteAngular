6dfcbe380e106ca21b80c17cafa4fe3d383bd839
jbayert <35811092+jbayert@users.noreply.github.com>
2020-05-10 22:02:15 -0500

    Broken Test

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -4,17 +4,37 @@ import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app.routing';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './paperkit/shared/navbar/navbar.component'
+import { NavbarComponent } from './main/navbar/navbar.component';
+import { LoginPageComponent } from './main/login-page/login-page.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxAuthFirebaseUIModule } from 'ngx-auth-firebaseui';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyB270i-oNVZK78Hn2nC0Va42NFcXTm0aEA",
+  authDomain: "party-games-ef841.firebaseapp.com",
+  databaseURL: "https://party-games-ef841.firebaseio.com",
+  projectId: "party-games-ef841",
+  storageBucket: "party-games-ef841.appspot.com",
+  messagingSenderId: "609024430601",
+  appId: "1:609024430601:web:5821481043cec4ba60ded8",
+  measurementId: "G-XSGCNK51BV"
+};
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
     RouterModule,
     AppRoutingModule,
+    NgxAuthFirebaseUIModule.forRoot(firebaseConfig),
+    BrowserAnimationsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Angular/src/app/app.routing.ts
+++ Angular/src/app/app.routing.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule, } from '@angular/common';
 import { BrowserModule  } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
-
+import { HomeComponent } from './main/home/home.component';
+import { LoginPageComponent } from './main/login-page/login-page.component'
 
 const routes: Routes =[
+  { path: 'login', component: LoginPageComponent },
+  { path: '', component: HomeComponent }
 ];
 
 @NgModule({
